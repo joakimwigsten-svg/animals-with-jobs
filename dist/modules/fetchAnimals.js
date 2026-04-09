98b68e35d6c1f80742d748b7c696b777d16f5a95
joakimwigsten-svg <joakim.wigsten@gmail.com>
2026-04-09 15:05:09 +0200

    Added fetch ´, installed npm , gitignore and interface

---
+++ dist/modules/fetchAnimals.js
@@ -0,0 +1,14 @@
+export default async function fetchAnimals() {
+    try {
+        const response = await fetch("/data/data.json");
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        const data = await response.json();
+        return data;
+    }
+    catch (error) {
+        console.error("Error fetching animals:", error);
+        return null;
+    }
+}
